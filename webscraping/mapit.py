9d3dc55f073fb07a7491f6a91be3bf0928671446
Roydan Chu <roydanchu@gmail.com>
2020-12-14 10:29:57 -0800

    Updated mapit.py

--- webscraping/mapit.py
+++ webscraping/mapit.py
@@ -2,11 +2,18 @@
 # browser using an address from the command line
 
 import webbrowser, sys
+import pyperclip
 
+prefix = "https://www.google.com/maps/place/"
+# If there are arguements, use args as address
+# If there are no arguments, grab from clipboard
 if len(sys.argv) > 1:
-    arguments = " ".join(sys.argv[1:])
-    prefix = "https://www.google.com/maps/place/"
+    name = 1
+    address = " ".join(sys.argv[1:])
 
+else:
+    address = pyperclip.paste()
     webbrowser.open(prefix + address)
 
+webbrowser.open(prefix + address)
 # TODO: Get the address from the cliboard.
