2e830fba69aa94a5d556b7c711353c2c6ed62614
Roydan Chu <roydanchu@gmail.com>
2021-01-13 10:30:34 -0800

    Created second block

--- objects/Pygame/dvd_image_moving.py
+++ objects/Pygame/dvd_image_moving.py
@@ -9,6 +9,7 @@
 #   * draw a rectangle on the screen
 #   * move the rectangle in x and y
 
+import random
 import pygame
 
 # ----- CONSTANTS
@@ -28,6 +29,7 @@ class Rectangle:
         self.colour = colour
 
         self.vel_x = 3
+        self.vel_y = 3
 
     def draw(self, screen):
         pygame.draw.rect(
@@ -40,6 +42,22 @@ class Rectangle:
                 self.height
             ]
         )
+
+    def update(self):
+        """Updates the location of the block in space.
+
+        Returns:
+            None
+        """
+
+        self.x += self.vel_x
+        self.y += self.vel_y
+
+        # Keep it in the screen
+        if self.x + self.width > WIDTH or self.x < 0:
+            self.vel_x *= -1
+        if self.y + self.height > HEIGHT or self.y < 0:
+            self.vel_y *= -1
 
 def main():
     pygame.init()
@@ -54,6 +72,15 @@ def main():
     clock = pygame.time.Clock()
 
     block_one = Rectangle((0, 255, 0))
+    block_two = Rectangle()
+    block_two.x, block_one.y = (
+        random.randrange(0, WIDTH-block_two.width),
+        random.randrange(0, HEIGHT-block_two.height)
+    )
+    block_two.vel_x, block_two.vel_y = (
+        random.choice([-4, -3, 3, 4]),
+        random.choice([-4, -3, 3, 4])
+    )
 
     # ----- MAIN LOOP
     while not done:
@@ -63,10 +90,14 @@ def main():
                 done = True
 
         # ----- LOGIC
+        # update the position of the block
+        block_one.update()
+        block_two.update()
 
         # ----- DRAW
         screen.fill(BLACK)
         block_one.draw(screen)
+        block_two.draw(screen)
 
         # ----- UPDATE
         pygame.display.flip()
